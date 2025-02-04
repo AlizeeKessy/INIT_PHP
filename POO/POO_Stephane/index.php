a40470d2288ab51680bc35ce47fbd506ccc3e9b6
AKABI NANGA André <andreparfaitakabinanga@gmail.com>
2025-02-04 15:43:01 +0000

    INIT_POO_EXO_STEPHANE

---
+++ POO/POO_Stephane/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once "classes/Compte.php";
+
+//On instancie le Compte
+$compte1 = new Compte('arg1', 'arg2'); // Replace 'arg1' and 'arg2' with actual arguments
+var_dump($compte1);
+?>
